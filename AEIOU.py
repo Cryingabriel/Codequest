7f94cbaa9350794dc1364553b9d03afee9c1c993
Cryingabriel <112021236+Cryingabriel@users.noreply.github.com>
2023-10-09 14:38:45 -0600

    Update AEIOU.py

--- AEIOU.py
+++ AEIOU.py
@@ -1,14 +1,15 @@
 import sys
-num = 0
+
 cases = (int(sys.stdin.readline().rstrip()))
 
 
 
 for i in range(cases):
     line = sys.stdin.readline().rstrip()
+    num = 0
+    f = len(line)
+    for i in range(f): 
+       if line[i] == "a" or line[i] == "e" or line[i] == "i" or line[i] == "o" or  line[i] =="u":
+            num += 1
 
-    line = line.split(" ")
-    if len(line) == "a" or "e" or "i" or "o" or  "u":
-        num += 1
-
-print(num)
+    print(num)
