a3dcb77d20aa99a176992a3b03cd8f250fed0b01
Cryingabriel <Cai19mac@gmail.com>
2024-02-28 13:45:05 -0700

    not submitted

---
+++ No more shouting.py
@@ -0,0 +1,4 @@
+cases = int(input().rstrip())
+for i in range(cases):
+    line = input().rstrip()
+    print(line.lower())
---
+++ letter counter.py
@@ -0,0 +1,4 @@
+cases = int(input().rstrip())
+for i in range(cases):
+    line = input().rstrip()
+    print(len(line)+1)
